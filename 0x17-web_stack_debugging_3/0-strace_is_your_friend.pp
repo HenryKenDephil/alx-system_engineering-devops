35efe528e669817e7668b3f24e4ffeb1621381d1
HenryKenDephil <h.komoloh@gmail.com>
2023-01-18 11:47:03 +0300

    update trace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,8 +2,7 @@
 
 # script to fix file .phpp to .php
 
-exec{
-	'fix-file':
-		command => 'sed -i s/phpp/g/var/www/html/wp-seetings.php',
-		...path => '/bin/',
-} 
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
